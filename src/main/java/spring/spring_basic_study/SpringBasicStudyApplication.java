e853e4db7233426b79e9ce6a38b695cc86b114ce
ethany00 <effervescent.yoo@gmail.com>
2024-10-20 00:15:32 +0900

    setting

---
+++ src/main/java/spring/spring_basic_study/SpringBasicStudyApplication.java
@@ -0,0 +1,13 @@
+package spring.spring_basic_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/spring_basic_study/SpringBasicStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.spring_basic_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
