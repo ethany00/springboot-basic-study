0fc459e70522ebeea72fceb65bc3142c402247ee
ethany00 <effervescent.yoo@gmail.com>
2024-10-20 23:31:51 +0900

    회원 조회

--- src/main/java/spring/spring_basic_study/controller/MemberController.java
+++ src/main/java/spring/spring_basic_study/controller/MemberController.java
@@ -2,10 +2,13 @@ package spring.spring_basic_study.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import spring.spring_basic_study.domain.Member;
 import spring.spring_basic_study.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -34,6 +37,13 @@ public class MemberController {
         System.out.println("member = " + member.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 
 }
