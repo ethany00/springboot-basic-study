01c32896cbb858027d7d5075a32e600e8bebedff
ethany00 <effervescent.yoo@gmail.com>
2024-10-20 16:36:49 +0900

    controller api 방식 예제

--- src/main/java/spring/spring_basic_study/controller/HelloController.java
+++ src/main/java/spring/spring_basic_study/controller/HelloController.java
@@ -3,6 +3,8 @@ package spring.spring_basic_study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,6 +15,41 @@ public class HelloController {
 
         // resource -> templates -> hello.html 찾음
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name,Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloMvc(@RequestParam(value = "name") String name){
+        // viewResolver 동작
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value = "name") String name){
+        // ResponseBody가 존재 -> HttpMessageConverter 동작
+        // 객체이면 -> jsonConverter 동작 | string이면 -> stringConverter 동작
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
